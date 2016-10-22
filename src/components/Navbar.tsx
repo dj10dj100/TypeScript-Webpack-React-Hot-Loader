10cf8b97cb7b7b361d8a4e115888584334d34147
daniel jenkins <dan@daniel-jenkins.com>
2016-10-22 12:06:32 +0200

    required reps and loaders.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,9 @@ export interface NavbarProps {
 
 export class Navbar extends React.Component<NavbarProps, {}> {
     render() {
-        return <h1>HMR should update {this.props.compiler} and {this.props.framework}!</h1>;
+        return <div>
+                    <h1>HMR should update {this.props.compiler} and {this.props.framework}!</h1>
+                    Make a small change and watch everything update.
+                </div>;
     }
 }
